8442821096e78972b9245a35317c98355ef81c6c
Marv <mrvncaragay@gmail.com>
2021-04-04 17:08:06 -0700

    Add store icon

--- src/screens/Store/SearchStore.js
+++ src/screens/Store/SearchStore.js
@@ -1,9 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
-import { Surface, Text, TextInput, TouchableRipple, IconButton, useTheme } from 'react-native-paper';
+import { Surface, Text, TextInput, TouchableRipple, IconButton, useTheme, Avatar } from 'react-native-paper';
 import { useStateValue } from '../../context';
 import { storeData } from '../../utils/asyncStorage';
 import firebase from '../../firebase';
+
+const storeUrls = {
+	walmart:
+		'https://d2d8wwwkmhfcva.cloudfront.net/40x/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/1487/5ffe3fb7-2a0c-4714-8c71-364d7186a3d3.png',
+	raleys:
+		'https://d2d8wwwkmhfcva.cloudfront.net/156x/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/291/63d7cd94-e679-431e-becf-208a02d6dd17.png',
+	safeway:
+		'https://d2d8wwwkmhfcva.cloudfront.net/156x/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/1/d2240f3c-34d1-4d25-8653-77213b4bd41c.png',
+	'lucky-supermarkets':
+		'https://d2d8wwwkmhfcva.cloudfront.net/156x/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/542/930560f2-a134-496c-a71b-99984d9bfa52.png',
+	foodmaxx:
+		'https://d2d8wwwkmhfcva.cloudfront.net/156x/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/611/e41762fd-41c5-4f79-a202-4ea8df5020ab.png',
+	target:
+		'https://d2d8wwwkmhfcva.cloudfront.net/156x/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/648/e1c282e5-21e7-4ebb-8936-cf23fbec29d9.png',
+	costco:
+		'https://d2d8wwwkmhfcva.cloudfront.net/156x/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/5/65f2304b-908e-4cd0-981d-0d4e4effa8de.png',
+};
 
 const Store = ({ store, navigation }) => {
 	const [_, dispatch] = useStateValue();
@@ -35,7 +52,14 @@ const Store = ({ store, navigation }) => {
 			}}
 		>
 			<Surface style={styles.store}>
-				<View>
+				<Avatar.Image
+					size={36}
+					style={{ backgroundColor: '#fff' }}
+					source={{
+						uri: storeUrls[store.name.toLowerCase()],
+					}}
+				/>
+				<View style={{ flex: 1, marginLeft: 10 }}>
 					<Text>{`${store.name} - ${store.storeNumber}`}</Text>
 					<Text style={{ color: 'gray' }}>{`${store.city}, ${store.state}`}</Text>
 				</View>
@@ -98,7 +122,6 @@ const styles = StyleSheet.create({
 		elevation: 2,
 		margin: 1,
 		alignItems: 'center',
-		justifyContent: 'space-between',
 		flexDirection: 'row',
 		padding: 15,
 		paddingRight: 0,
